26c49d3b1914ccebf0d169b5fbe913277ce91384
jmejiamu <jmejiamu@gmail.com>
2020-08-27 19:45:17 -0700

    setting-up-login-and-register-form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
 // Components
 import NavBar from './components/NavBar';
 import MenuItems from './components/MenuItems';
 import Events from './components/Events';
 import Assistance  from './components/Assisstance/Assistance';
+import Login  from './components/LoginLogout/Login';
 // import PageNotFound from './components/PageNotFound';
 
 const App = () => {
@@ -17,7 +17,8 @@ const App = () => {
 
         <NavBar />
         <Switch>
-          <Route exact path='/' component={MenuItems} />
+          <Route exact={true} path='/' component={Login} />
+          <Route exact path='/menu' component={MenuItems} />
           <Route exact path='/events' component={Events} />
           <Route exact path='/assistance' component={Assistance} />
           {/* <Route exact path='/events/:id' component={PageNotFound} /> */}
---
+++ src/components/LoginLogout/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <h1 className="events-section">Log In</h1>
+        </div>
+    )
+}
+
+export default Login
