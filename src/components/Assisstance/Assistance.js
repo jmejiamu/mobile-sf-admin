a984f6d5ab98407caa9b267ee805610485c58cd4
Jose <jmejiamu@gmail.com>
2020-09-01 17:14:26 -0700

    adding-login-inputs

--- src/components/Assisstance/Assistance.js
+++ src/components/Assisstance/Assistance.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect}from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
@@ -8,7 +8,7 @@ const Assistance = () => {
 
     const deleteAssistance = async (id) => {
         try {
-            const deleteData = await fetch(`http://157.245.184.202:8080/deleteAssistance/${id}`,{
+            const deleteData = await fetch(`http://157.245.184.202:8080/deleteAssistance/${id}`, {
                 method: 'DELETE'
             })
             setAssistanceData(assistanceData.filter(assistanceToDelete => assistanceToDelete.id !== id))
@@ -22,25 +22,25 @@ const Assistance = () => {
         try {
             const response = await fetch('http://157.245.184.202:8080/assistancedata')
             const jsonData = await response.json();
-        
+
             setAssistanceData(jsonData);
-            
+
         } catch (error) {
             console.error(error);
         };
-       
+
     };
 
-    useEffect(() =>{
+    useEffect(() => {
         getData();
     }, [])
-    return(
+    return (
         <div>
             <h1 className="events-section">Assistance Screen</h1>
-            {assistanceData === 0  ? <h1 className="text-center mt-5 mb-5 text-white">There is not events yet!{'😌'}</h1> : (
+            {assistanceData === 0 ? <h1 className="text-center mt-5 mb-5 text-white">There is not events yet!{'😌'}</h1> : (
                 assistanceData.map((assistance) => {
-                    return(
-                        <div className="card mb-5"  key={assistance.id}>
+                    return (
+                        <div className="card mb-5" key={assistance.id}>
                             <div className="card-body text-left">
                                 <h5 className="card-title" ><strong>Name:</strong> {assistance.full_name}</h5>
                                 <p className="card-text" > <strong>Notes:</strong>  {assistance.notes}</p>
@@ -48,9 +48,9 @@ const Assistance = () => {
                                 <p className="card-text" ><strong>Phone:</strong> {assistance.phone}</p>
                                 <p className="card-text" ><strong>Email:</strong> {assistance.email}</p>
                                 <button
-                                 type="button"
-                                 className="card-link btn btn btn-danger"
-                                 onClick={()=> deleteAssistance(assistance.id)}
+                                    type="button"
+                                    className="card-link btn btn btn-danger"
+                                    onClick={() => deleteAssistance(assistance.id)}
                                 >DELETE</button>
 
                             </div>
--- src/components/LoginLogout/Login.js
+++ src/components/LoginLogout/Login.js
@@ -4,6 +4,21 @@ const Login = () => {
     return (
         <div>
             <h1 className="events-section">Log In</h1>
+            <label className="text-white">User</label>
+            <input
+                type="text"
+                className="form-control"
+            // value={startData}
+            // onChange={e => setStartData(e.target.value)} 
+            />
+            <label className="text-white">Password</label>
+            <input
+                type="text"
+                className="form-control"
+            // value={startData}
+            // onChange={e => setStartData(e.target.value)} 
+            />
+
         </div>
     )
 }
