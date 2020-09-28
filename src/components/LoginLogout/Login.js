9f157c7be5dd907fa1bc808123919ca5ece70e0f
Jose <jmejiamu@gmail.com>
2020-09-27 21:26:01 -0700

    post-for-login

--- src/components/LoginLogout/Login.js
+++ src/components/LoginLogout/Login.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitUserData = (e) => {
+
+        axios.post('http://localhost:3001/signin', { email, password })
+            .then(response => {
+                if (response.data.id) {
+                    window.location = '/menu'
+                }
+            })
+
+        // window.location = '/menu'
+    }
+
+
     return (
         <div>
             <h1 className="events-section">Log In</h1>
@@ -9,18 +28,22 @@ const Login = () => {
                 type="text"
                 placeholder="User name"
                 className="form-control"
-            // value={startData}
-            // onChange={e => setStartData(e.target.value)} 
+                value={email}
+                onChange={e => setEmail(e.target.value)}
             />
             <label className="text-white">Password</label>
             <input
-                type="text"
+                type="password"
                 placeholder="Password"
                 className="form-control"
-            // value={startData}
-            // onChange={e => setStartData(e.target.value)} 
+                value={password}
+                onChange={e => setPassword(e.target.value)}
             />
-            <button type="button" className="btn btn-danger btn-sm mt-5">Log In</button>
+            <button
+                type="button"
+                className="btn btn-danger btn-sm mt-5"
+                onClick={submitUserData}>Log In</button>
+
             <button type="button" className="btn btn-danger btn-sm mt-5">Register</button>
 
         </div>
