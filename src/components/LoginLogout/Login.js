2fa2485a464e8f16e0853ed7515354d44006d9b5
Jose <jmejiamu@gmail.com>
2020-09-28 23:01:51 -0700

    added-register-fetch

--- src/components/LoginLogout/Login.js
+++ src/components/LoginLogout/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, } from 'react';
 
 
 import axios from 'axios';
@@ -9,23 +9,46 @@ const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const submitUserData = (e) => {
-
-        axios.post('http://localhost:3001/signin', { email, password })
-            .then(response => {
-                console.log(response);
-                if (response.data.id) {
-                    props.history.push('/menu')
-                }
-            }).catch((error) => {
-                alert('wrong credential')
-                // console.log(response)
-            })
-
-
-        // window.location = '/menu'
+    const goToRegister = () => {
+        props.history.push('/register')
     }
 
+
+    const submitUserData = async (e) => {
+
+        // axios.post('http://localhost:3001/signin', { email, password })
+        //     .then(response => {
+        //         console.log(response.json());
+        //         if (response.data.id) {
+        //             props.history.push('/menu')
+        //         }
+        //     }).catch((error) => {
+        //         alert('wrong credential')
+        //         // console.log(response)
+        //     })
+
+        try {
+            const body = {
+                email: email,
+                password: password
+            }
+
+            const response = await fetch('http://localhost:3001/signin', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(body)
+            })
+
+            const data = await response.json()
+            console.log(data);
+            if (data.id) {
+                props.history.push('/menu')
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <div>
@@ -51,7 +74,11 @@ const Login = (props) => {
                 className="btn btn-danger btn-sm mt-5"
                 onClick={submitUserData}>Log In</button>
 
-            <button type="button" className="btn btn-danger btn-sm mt-5">Register</button>
+            <button
+                type="button"
+                className="btn btn-danger btn-sm mt-5"
+                onClick={goToRegister}
+            >Register</button>
 
         </div>
     )
--- src/components/LoginLogout/Register.js
+++ src/components/LoginLogout/Register.js
@@ -1,6 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 const Register = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitUserData = async () => {
+
+        // const body = {
+        //     name: name,
+        //     email: email,
+        //     password: password
+        // }
+        // axios({
+        //     method: 'POST',
+        //     url: 'http://localhost:3001/register',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     data: body
+        // })
+
+        try {
+            const body = {
+                name: name,
+                email: email,
+                password: password
+            }
+
+            const response = await fetch('http://localhost:3001/register', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'same-origin',
+                body: JSON.stringify(body)
+            })
+            const data = await response.headers.get('jwt')
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+
+
+
+    }
+
+
     return (
         <div>
             <h1 className="events-section">Register</h1>
@@ -10,8 +60,8 @@ const Register = () => {
                 type="text"
                 placeholder="User name"
                 className="form-control"
-            // value={startData}
-            // onChange={e => setStartData(e.target.value)} 
+                value={name}
+                onChange={e => setName(e.target.value)}
             />
 
             <label className="text-white">Email</label>
@@ -19,8 +69,8 @@ const Register = () => {
                 type="text"
                 placeholder="Email"
                 className="form-control"
-            // value={startData}
-            // onChange={e => setStartData(e.target.value)} 
+                value={email}
+                onChange={e => setEmail(e.target.value)}
             />
 
             <label className="text-white">Password</label>
@@ -28,10 +78,14 @@ const Register = () => {
                 type="text"
                 placeholder="Password"
                 className="form-control"
-            // value={startData}
-            // onChange={e => setStartData(e.target.value)} 
+                value={password}
+                onChange={e => setPassword(e.target.value)}
             />
-            <button type="button" className="btn btn-danger btn-sm mt-5">Register</button>
+            <button
+                type="button"
+                className="btn btn-danger btn-sm mt-5"
+                onClick={submitUserData}
+            >Register</button>
         </div>
     );
 };
