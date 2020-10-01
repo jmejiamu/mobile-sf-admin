20f709e27e8309a1e911f8f99778faa8de822f02
Jose <jmejiamu@gmail.com>
2020-09-30 20:17:21 -0700

    added-redirect-route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 // Components
 import NavBar from './components/NavBar';
@@ -11,6 +11,9 @@ import Login from './components/LoginLogout/Login';
 import Register from './components/LoginLogout/Register';
 
 const App = () => {
+
+  const [isAuthenticated, setAuthenticated] = useState(false);
+
   return (
     <BrowserRouter>
 
@@ -18,12 +21,18 @@ const App = () => {
 
         <NavBar />
         <Switch>
-          <Route exact={true} path='/' component={Login} />
+          {/* <Route exact={true} path='/' component={Login} />
           <Route exact path='/menu' component={MenuItems} />
           <Route exact path='/events' component={Events} />
           <Route exact path='/register' component={Register} />
-          <Route exact path='/assistance' component={Assistance} />
+          <Route exact path='/assistance' component={Assistance} /> */}
           {/* <Route exact path='/events/:id' component={PageNotFound} /> */}
+          {/* '/'this would be the login  */}
+          <Route exact={true} path='/' render={props => !isAuthenticated ? <Login {...props} /> : <Redirect to="/menu" />} />
+          <Route exact path='/menu' render={props => isAuthenticated ? < MenuItems {...props} /> : <Redirect to="/" />} />
+          <Route exact path='/events' render={props => isAuthenticated ? <Events {...props} /> : <Redirect to="/" />} />
+          <Route exact path='/register' render={props => !isAuthenticated ? <Register {...props} /> : <Redirect to="/" />} />
+          <Route exact path='/assistance' render={props => isAuthenticated ? <Assistance {...props} /> : <Redirect to="/" />} />
         </Switch>
 
       </div>
