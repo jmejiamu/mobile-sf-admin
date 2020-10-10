26df4fd9d6bbefd3c1080eda96ce4d838edbc58b
Jose <jmejiamu@gmail.com>
2020-10-10 15:05:19 -0700

    retrieves-art-data

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,9 +1,45 @@
 import React, { useState, useEffect } from 'react'
 
 const Art = (props) => {
+    const [artData, setArtData] = useState([]);
+
+    const getArt = async () => {
+        try {
+            const response = await fetch('http://157.245.184.202:8080/arts')
+            const jsonData = await response.json()
+            setArtData(jsonData);
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    useEffect(() => {
+        getArt();
+    })
+
     return (
         <div>
-            <h1>Art Section</h1>
+            <h1 className="text-white">Art Section</h1>
+
+            {artData.length === 0 ? <h1 className="text-center mt-5 mb-5 text-white">There is not art piece yet {'😌'} </h1> : (
+                artData.map(art => {
+                    return (
+                        <div className="card mb-5" key={art.id}>
+                            <div className="card-body text-left" >
+                                <img className="card-img-top" src={art.path} alt="image" />
+                                <h5 className="card-title" >{art.title}</h5>
+                                {/* <p>Image url: {art.path}</p> */}
+                                <p><strong>Cost: </strong> {art.details}</p>
+                                <p><strong>Details: </strong>{art.contact}</p>
+                                <p><strong>User name:</strong> {art.name}</p>
+                                <p><strong>User' Bid: </strong>{art.bid}</p>
+                                <p><strong>Phone or Email:  </strong>{art.phone_email}</p>
+                            </div>
+
+                        </div>
+                    )
+                })
+            )}
         </div>
     )
 }
