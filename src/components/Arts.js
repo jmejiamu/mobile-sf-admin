ed143311f8a04365f96a54ec07bb56f31a5fc0ea
Jose Mejia <jmejiamu@gmail.com>
2020-10-20 16:11:22 -0700

    Update Arts.js

--- src/components/Arts.js
+++ src/components/Arts.js
@@ -8,7 +8,7 @@ const Arts = (props) => {
     const [artData, setArtData] = useState([]);
 
     const getArt = async () => {
-        try { 
+        try {
             const response = await fetch('http://157.245.184.202:8080/arts')
             const jsonData = await response.json()
             setArtData(jsonData);
