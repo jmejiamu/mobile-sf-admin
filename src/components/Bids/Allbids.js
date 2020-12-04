24235e72dc2cb2f8a1307bc8aebca080f4469d56
Jose <jmejiamu@gmail.com>
2020-12-03 17:25:49 -0800

    retrieving-data-bids

--- src/components/Bids/Allbids.js
+++ src/components/Bids/Allbids.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import NavBar from '../NavBar';
 
 const Allbids = () => {
+    const [bidData, setBidData] = useState([])
+    const getBids = async () => {
+        try {
+            const response = await fetch('http://157.245.184.202:8080/bids')
+            const jsonData = await response.json()
+            setBidData(jsonData)
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getBids();
+    }, [])
     return (
         <div>
-
+            <NavBar />
+            <h1 style={{ marginTop: 60 }} className="text-white">Bids</h1>
+            {bidData && bidData.map(bid => {
+                return (
+                    <div>
+                        <p className="text-white">
+                            {`Title: ${bid.title} 
+                            User Name: ${bid.name}
+                            Contact Info: ${bid.phone_email}
+                            Bid: ${bid.bid}`}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 };
