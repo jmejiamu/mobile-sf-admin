bfb7039e27fd0c7a09b4c0a7c9251ff836bd5125
Jose <jmejiamu@gmail.com>
2020-12-03 19:44:34 -0800

    added-table-to-show-bids-organized

--- src/components/Bids/Allbids.js
+++ src/components/Bids/Allbids.js
@@ -20,7 +20,7 @@ const Allbids = () => {
         <div>
             <NavBar />
             <h1 style={{ marginTop: 60 }} className="text-white">Bids</h1>
-            {bidData && bidData.map(bid => {
+            {/* {bidData && bidData.map(bid => {
                 return (
                     <div>
                         <p className="text-white">
@@ -30,7 +30,35 @@ const Allbids = () => {
                             Bid: ${bid.bid}`}</p>
                     </div>
                 )
-            })}
+            })} */}
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">User Name</th>
+                        <th scope="col">Contact Info</th>
+                        <th scope="col">Bid</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {bidData && bidData.map(bid => {
+                        return (
+
+                            <tr>
+                                <th className="text-white" scope="row">{bid.id}</th>
+                                <td className="text-white">{bid.title}</td>
+                                <td className="text-white" >{bid.name}</td>
+                                <td className="text-white " >{bid.phone_email}</td>
+                                <td className="text-white" >{bid.bid}</td>
+                            </tr>
+                        )
+                    })
+
+                    }
+
+                </tbody>
+            </table>
         </div>
     );
 };
